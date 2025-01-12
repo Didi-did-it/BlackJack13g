cac94cb82db861370550d25cb163964c7d84890e
Kilian Lorenz <kilian.lorenz@stud.fh-campuswien.ac.at>
2025-01-12 19:48:38 +0100

    Added different Screens and started

--- src/main/java/com/example/blackjack13g/App.java
+++ src/main/java/com/example/blackjack13g/App.java
@@ -11,9 +11,13 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("StartScene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+        Scene scene = new Scene(fxmlLoader.load());
+
+
         stage.setTitle("Blackjack by 13G");
         stage.setScene(scene);
+        stage.setFullScreen(true);
+
         stage.show();
     }
 
