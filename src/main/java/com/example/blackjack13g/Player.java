18310850e9771d3ae181537f0cf3d9693ca78919
Kilian Lorenz <kilian.lorenz@stud.fh-campuswien.ac.at>
2025-01-12 19:39:12 +0100

    Added different Screens and started

--- src/main/java/com/example/blackjack13g/Player.java
+++ src/main/java/com/example/blackjack13g/Player.java
@@ -1,4 +1,11 @@
 package com.example.blackjack13g;
 
+import java.util.ArrayList;
+
 public class Player {
+    private int score;
+    private  int health;
+    private ArrayList<Card> hand;
+
+
 }
---
+++ src/main/java/com/example/blackjack13g/StartController.java
@@ -0,0 +1,22 @@
+package com.example.blackjack13g;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class StartController {
+    public Button startButton;
+    public Button rulesButton;
+    public Button exitButton;
+
+    //Scene Control
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+    @FXML
+    private Parent root;
+}
