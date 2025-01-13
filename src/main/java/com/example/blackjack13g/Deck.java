b39d024e202815d96a9db6a0a85ea8c2533168e1
Diart Tahiri <diart.tahiri@hotmail.com>
2025-01-13 07:04:14 +0100

    added constructors to Deck, Card and Player added getter methods were possibly needed added draw method to Deck added addCard(), clearHand(), loseHP() and calculateScore() to Player

--- src/main/java/com/example/blackjack13g/Deck.java
+++ src/main/java/com/example/blackjack13g/Deck.java
@@ -1,4 +1,25 @@
 package com.example.blackjack13g;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
+    private ArrayList<Card> deck = new ArrayList<>();
+
+    public Deck() {
+        // 2 - 10 are the number cards, 11 - 13 Jack to King, and 14 is Ace
+        int[] values = new int[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+        for (int i = 0; i < 4; i++) {
+            for (int value : values) {
+                deck.add(new Card(value));
+            }
+        }
+
+        Collections.shuffle(deck);
+    }
+
+    public Card draw() {
+        return deck.remove(0);
+    }
 }
